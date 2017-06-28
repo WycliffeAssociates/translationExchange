4e7e5441ba0463c2ea34e3c952e0c01b1e5c18bd
ericaz98 <ez8@rice.edu>
2017-06-28 10:41:37 -0400

    more navbar edits

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,3 +1,29 @@
-/**
- * Created by ericazhong on 6/28/17.
- */
+import React, { Component } from 'react';
+import {
+    Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink, Container, Row, Col,
+    Jumbotron, InputGroup, Input, InputGroupAddon,} from 'reactstrap';
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                this is the about page.
+            </div>
+        );
+    }
+}
+
+export default About;
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,3 +1,44 @@
-/**
- * Created by ericazhong on 6/28/17.
- */
+import React, { Component } from 'react';
+import {
+    Container, Row, Col, Jumbotron, InputGroup, Input, InputGroupAddon, InputGroupButton,
+} from 'reactstrap';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <Container>
+                        <Row>
+                            <Col>
+                            <h1>Welcome to Translation Manager</h1>
+
+                             </Col>
+                        </Row>
+                <InputGroup>
+                <Input placeholder="username" />
+                    <InputGroupButton color="success">Submit</InputGroupButton>
+                {/*<InputGroupAddon>Submit</InputGroupAddon>*/}
+                </InputGroup>
+
+                </Container>
+                </Jumbotron>
+            </div>
+        );
+    }
+}
+
+export default Home;
