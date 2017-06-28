4338558efc5fe3378e21842df8594a177b1b4a73
sheesania <sheesania@users.noreply.github.com>
2017-06-28 09:41:47 -0400

    basic components for displaying chunks for a chapter

--- src/chapter/ChapterContainer.js
+++ src/chapter/ChapterContainer.js
@@ -1,9 +1,43 @@
 import React, {Component} from 'react';
+import ChunkList from "./ChunkList";
 
 class ChapterContainer extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {segments: [], mode: ""};
+    }
+
+    componentDidMount () {
+        var chunkID = this.props.match.params.chid;
+        //do a web request here for segments (chunks or verses) of chapter...
+        this.setState(
+            {
+                segments: [
+                    {
+                        id: 1,
+                        number: 1,
+                        takes: [{}, {}, {}]
+                    },
+                    {
+                        id: 2,
+                        number: 3,
+                        takes: [{}, {}]
+                    }
+                ],
+                mode: "chunk"
+            }
+        );
+    }
+
     render () {
         return (
-            <div>I'm a chapter container for {this.props.match.params.chid}!</div>
+            <div>
+                I'm a chapter container for {this.props.match.params.chid}!
+                <ChunkList
+                    segments={this.state.segments}
+                    mode={this.state.mode}
+                />
+            </div>
         );
     }
 }
---
+++ src/chapter/ChunkList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class ChunkList extends Component {
+    render () {
+        return (
+            <ul>
+                {this.props.segments.map(this.createListItem.bind(this))}
+            </ul>
+        );
+    }
+
+    createListItem (segment) {
+        var modeLabel = "";
+        switch (this.props.mode) {
+            case "verse":
+                modeLabel = "Verse";
+                break;
+            case "chunk":
+                modeLabel = "Chunk";
+                break;
+        }
+        return (
+            <li key={segment.id}>
+                Displaying takes for {modeLabel} {segment.number}...
+            </li>
+        );
+    }
+}
+
+export default ChunkList;
