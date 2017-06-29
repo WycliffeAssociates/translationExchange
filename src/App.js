27e37e4e2e0ce96f1b3f99adb7ba836a73acd4b8
Dennis Marchuk <dmarchuk@eagles.bridgewater.edu>
2017-06-29 09:24:50 -0400

    Stars
    Stars added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import AudioPlayer from './MainPage_Components/Audio_Player/audioPlayer';
 import SearchBar from './MainPage_Components/Search_Bar/search_bar';
+import ReactStars from 'react-stars'
+import { render } from 'react-dom'
+
+var ReactDOM = require('react-dom');
+var stars;
+var starAmount;
+var dbStar
+// const ratingChanged = (newRating) => {
+//     console.log(newRating)
+// }
+// render(<ReactStars count={3} onChange={ratingChanged} size={24} color2={'#ffd700'} half={false} />, document.getElementById('h1Test'));
+// stars = new ReactStars();
+//
+// //starAmount = stars.clicked(this.setState)
+// starCount = stars.getRate();//starCount is the amount of stars selected.
+// console.log(starCount);
+// //https://yarnpkg.com/en/package/react-stars-ie# ----- this is package info for ReactStars
+//
 
 
 class App extends Component {
@@ -35,5 +53,25 @@ class App extends Component {
     );
   }
 }
+
+
+Star();
+function Star(){
+
+    stars = new ReactStars();
+    dbStar = 2; //TODO this will have to be changed to whatever we get from the database
+    const ratingChanged = (newRating) => {//when the rating is changed it is logged here
+        console.log(newRating)
+    }
+
+    render(<ReactStars count={3} onChange={ratingChanged} size={24} color2={'#ffd700'} half={false} value={dbStar}/>, document.getElementById('stars'));
+
+
+    //if it does not work run this. Run npm first, only run yarn if npm does not work
+    //npm install react-stars --save
+    //yarn add react-stars-ie
+    //https://www.npmjs.com/package/react-stars-ie
+}
+
 
 export default App;
