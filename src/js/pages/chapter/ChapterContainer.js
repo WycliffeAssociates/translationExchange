7eb8074d08cdfa45aa61064ed6ed82fc19231e22
sheesania <sheesania@users.noreply.github.com>
2017-06-30 11:59:52 -0400

    new demo audio files

--- src/js/pages/chapter/ChapterContainer.js
+++ src/js/pages/chapter/ChapterContainer.js
@@ -17,25 +17,25 @@ class ChapterContainer extends Component {
                     {
                         mode: "chunk",
                         number: 1,
-                        sourceAudio: "https://files.freemusicarchive.org/music%2Fno_curator%2FThe_Womb%2FBang_-_An_Introduction_to_The_Womb%2FThe_Womb_-_02_-_Sex_Club.mp3",
+                        sourceAudio: "http://localhost:3001/files/Matthew_1_English.mp3",
                         takes: [
                             {
                                 id: 1,
-                                audioSource: "audiosource",
+                                audioSource: "http://localhost:3001/files/Matthew_1_Arabic_Iraqi.mp3",
                                 author: "Bob the Translator",
                                 rating: 3,
                                 timestamp: "timestamp"
                             },
                             {
                                 id: 2,
-                                audioSource: "audiosource",
+                                audioSource: "http://localhost:3001/files/Matthew_1_Arabic_VanDyke.mp3",
                                 author: "Alice the Translator",
                                 rating: 2,
                                 timestamp: "timestamp"
                             },
                             {
                                 id: 5,
-                                audioSource: "audiosource",
+                                audioSource: "http://localhost:3001/files/Matthew_1_Arabic_Sudanese.mp3",
                                 author: "Bob the Translator",
                                 rating: 3,
                                 timestamp: "timestamp"
@@ -45,18 +45,18 @@ class ChapterContainer extends Component {
                     {
                         mode: "chunk",
                         number: 3,
-                        sourceAudio: "https://files.freemusicarchive.org/music%2Fno_curator%2FThe_Womb%2FBang_-_An_Introduction_to_The_Womb%2FThe_Womb_-_02_-_Sex_Club.mp3",
+                        sourceAudio: "http://localhost:3001/files/Matthew_2_English.mp3",
                         takes: [
                             {
                                 id: 8,
-                                audioSource: "audiosource",
+                                audioSource: "http://localhost:3001/files/Matthew_2_Arabic_Iraqi.mp3",
                                 author: "Bob the Translator",
                                 rating: 1,
                                 timestamp: "timestamp"
                             },
                             {
                                 id: 10,
-                                audioSource: "https://files.freemusicarchive.org/music%2Fno_curator%2FThe_Womb%2FBang_-_An_Introduction_to_The_Womb%2FThe_Womb_-_02_-_Sex_Club.mp3",
+                                audioSource: "http://localhost:3001/files/Matthew_2_Arabic_VanDyke.mp3",
                                 author: "Alice the Translator",
                                 rating: 3,
                                 timestamp: "timestamp"
