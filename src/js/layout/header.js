78b5514439c08cc16b5af932b2f8cac30acd3d87
ericaz98 <ez8@rice.edu>
2017-07-10 16:44:24 -0400

    modified navbar to use semantic

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -1,56 +1,73 @@
 import React, { Component } from 'react';
 import {Nav, Navbar, NavDropdown, NavItem} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
+import {Menu} from "semantic-ui-react";
+import {Link} from "react-router-dom";
 
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
+    state = { activeItem: 'home' };
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
-    }
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+    //
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.toggle = this.toggle.bind(this);
+    //     this.state = {
+    //         isOpen: false
+    //     };
+    // }
+    // toggle() {
+    //     this.setState({
+    //         isOpen: !this.state.isOpen
+    //     });
+    // }
     render() {
+        // return (
+        //     <div>
+        //         <Navbar inverse collapseOnSelect>
+        //             <Navbar.Header>
+        //                 <Navbar.Brand>
+        //                     <LinkContainer to="/">
+        //                         <a>Translation Manager</a>
+        //                     </LinkContainer>
+        //                 </Navbar.Brand>
+        //                 <Navbar.Toggle />
+        //             </Navbar.Header>
+        //             <Navbar.Collapse>
+        //
+        //                 <Nav pullRight>
+        //                     <LinkContainer to="/about/">
+        //                         <NavItem eventKey={1}>About</NavItem>
+        //                     </LinkContainer>
+        //
+        //                     <LinkContainer to="/projects/">
+        //                         <NavItem eventKey={2}>Projects</NavItem>
+        //                     </LinkContainer>
+        //
+        //                 </Nav>
+        //
+        //
+        //             </Navbar.Collapse>
+        //         </Navbar>
+        //
+        //
+        //
+        //
+        //     </div>
+        // );
+
+        const { activeItem } = this.state;
+
         return (
-            <div>
-                <Navbar inverse collapseOnSelect>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <LinkContainer to="/">
-                                <a>Translation Manager</a>
-                            </LinkContainer>
-                        </Navbar.Brand>
-                        <Navbar.Toggle />
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-
-                        <Nav pullRight>
-                            <LinkContainer to="/about/">
-                                <NavItem eventKey={1}>About</NavItem>
-                            </LinkContainer>
-
-                            <LinkContainer to="/projects/">
-                                <NavItem eventKey={2}>Projects</NavItem>
-                            </LinkContainer>
-
-                        </Nav>
-
-
-                    </Navbar.Collapse>
-                </Navbar>
-
-
-
-
-            </div>
-        );
+            <Menu inverted size='massive'>
+                <Menu.Item as={Link} to='/' name='Home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+                <Menu.Item as={Link} to='/about/' name='About' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+                <Menu.Item as={Link} to='/projects/' name='Projects' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+            </Menu>
+        )
     }
 }
 
