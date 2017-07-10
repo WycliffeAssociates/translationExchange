2d48f8d8174bfc97f46d712d5e56f5969521fd1d
sheesania <sheesania@users.noreply.github.com>
2017-07-10 11:58:01 -0400

    finish loading display + retry functionality

--- src/js/pages/chapter/ChapterContainer.js
+++ src/js/pages/chapter/ChapterContainer.js
@@ -9,7 +9,7 @@ import config from '../../../config';
 class ChapterContainer extends Component {
     constructor (props) {
         super(props);
-        this.state = {loaded: false, retry: null, segments: [], mode: "", source: "", takeList: []};
+        this.state = {loaded: false, error: "", segments: [], mode: "", source: "", takeList: []};
     }
 
     componentDidMount () {
@@ -18,101 +18,21 @@ class ChapterContainer extends Component {
 
     requestData () {
         //var chapterID = this.props.match.params.chid;
-        //do a web request here for segments (chunks or verses) of chapter...
-    //     var self = this;
-    //     axios.get(config.apiUrl + 'takes')
-    //         .then(function (response) {
-    //             throw "mewy";
-    //             console.dir(response.data[0]);
-    //
-    //             self.setState(
-    //                 {
-    //                     loaded: true,
-    //                     retry: null,
-    //                     segments: [
-    //                         {
-    //                             mode: "chunk",
-    //                             number: 1,
-    //                             sourceAudio: config.streamingUrl + response.data[0].location,
-    //                             takes: [
-    //                                 {
-    //                                     id: 1,
-    //                                     audioSource: "http://localhost:3001/files/Matthew_1_Arabic_Iraqi.mp3",
-    //                                     author: "Bob the Translator",
-    //                                     rating: 3,
-    //                                     timestamp: "26 June 2017 8:44 pm"
-    //                                 },
-    //                                 {
-    //                                     id: 2,
-    //                                     audioSource: "http://localhost:3001/files/Matthew_1_Arabic_VanDyke.mp3",
-    //                                     author: "Alice the Translator",
-    //                                     rating: 2,
-    //                                     timestamp: "28 June 2017 10:07 am"
-    //                                 },
-    //                                 {
-    //                                     id: 5,
-    //                                     audioSource: "http://localhost:3001/files/Matthew_1_Arabic_Sudanese.mp3",
-    //                                     author: "Bob the Translator",
-    //                                     rating: 3,
-    //                                     timestamp: "28 June 2017 10:11 am"
-    //                                 }
-    //                             ]
-    //                         },
-    //                         {
-    //                             mode: "chunk",
-    //                             number: 3,
-    //                             sourceAudio: "http://localhost:3001/files/Matthew_2_English.mp3",
-    //                             takes: [
-    //                                 {
-    //                                     id: 8,
-    //                                     audioSource: "http://localhost:3001/files/Matthew_2_Arabic_Iraqi.mp3",
-    //                                     author: "Bob the Translator",
-    //                                     rating: 1,
-    //                                     timestamp: "June 3 2017 4:12 pm"
-    //                                 },
-    //                                 {
-    //                                     id: 10,
-    //                                     audioSource: "http://localhost:3001/files/Matthew_2_Arabic_VanDyke.mp3",
-    //                                     author: "Alice the Translator",
-    //                                     rating: 3,
-    //                                     timestamp: "June 3 2017 4:22 pm"
-    //                                 }
-    //                             ]
-    //                         }
-    //                     ]
-    //                 }
-    //             );
-    //         })
-    //         .catch(function (error) {
-    //             console.log(error);
-    //             self.setState( {retry: self.requestData} );
-    //             console.log(self.state.retry);
-    //         });
-    // }
-    //
-    // render () {
-    //     console.log("retry: " + this.state.retry);
-    //     return (
-    //         <div>
-    //             {/*Chapter {this.props.match.params.chid}*/}
-    //             <LoadingDisplay loaded={this.state.loaded} retry={this.state.retry}>
-    //                 <ChunkList
-    //                     segments={this.state.segments}
-    //                     mode={this.state.segments.mode}
-    //                 />
-    //             </LoadingDisplay>
+        this.setState({ error: "" });
         axios.get('http://172.19.145.91:8000/api/takes/', {
             params: {
                 chapter: 6
             }
         }).then((results) => {
-
             this.setState(
                 {
+                    loaded: true,
                     segments: results.data,
                     mode: results.data[0].mode
                 }
-            )
+            );
+        }).catch((exception) => {
+            this.setState({ error: exception });
         });
 
     }
@@ -123,12 +43,12 @@ class ChapterContainer extends Component {
 
         for (let i = 0; i < placeHolderArr.length; i++) {
 
-            newArr[newArr.length] = placeHolderArr[i].startv
+            newArr[newArr.length] = placeHolderArr[i].startv;
         }
 
         var uniqueArray = newArr.filter(function(item, pos) {
             return newArr.indexOf(item) === pos;
-        })
+        });
 
         return(
             uniqueArray
@@ -138,15 +58,15 @@ class ChapterContainer extends Component {
 
     // creates array containing each chunk and array of each take in that chunk
     createChunkTakes(placeHolderArr) {
-        var uniqueArray = this.findChunks(placeHolderArr)
+        var uniqueArray = this.findChunks(placeHolderArr);
         var finalArr = [];
         for(let i = 0; i < uniqueArray.length; i++) {
-            var counter = 0
-            var chunkArr = []
+            var counter = 0;
+            var chunkArr = [];
 
             for(let j = 0; j < placeHolderArr.length; j++) {
                 if (placeHolderArr[j].startv === uniqueArray[i]) {
-                    chunkArr[counter] = placeHolderArr[j]
+                    chunkArr[counter] = placeHolderArr[j];
                     counter += 1;
                 }
             }
@@ -169,14 +89,16 @@ class ChapterContainer extends Component {
     }
 
     render () {
-
-        var segments = this.state.segments
-        var finalArr = this.createChunkTakes(segments)
-
+        var segments = this.state.segments;
+        var finalArr = this.createChunkTakes(segments);
         return (
             <div>
                 <h1>Chapter {this.props.match.params.chid}</h1>
-                {finalArr.map(this.createChunkList)}
+                <LoadingDisplay loaded={this.state.loaded}
+                                retry={this.requestData.bind(this)}
+                                error={this.state.error}>
+                    {finalArr.map(this.createChunkList)}
+                </LoadingDisplay>
             </div>
         );
     }
