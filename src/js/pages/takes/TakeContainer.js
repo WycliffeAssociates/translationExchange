b2a48e1c11cd11b12fd28d66bd2adf1df69b0be8
ericaz98 <ez8@rice.edu>
2017-07-11 15:58:38 -0400

    button to display text that will be replaced w/ src audio

--- src/js/pages/takes/TakeContainer.js
+++ src/js/pages/takes/TakeContainer.js
@@ -5,10 +5,23 @@ import Star from './components/Star';
 import AudioComponent from './components/AudioComponent';
 import axios from 'axios';
 import config from "config/config";
+import {Button, Card, Divider, Grid, Segment} from "semantic-ui-react";
 
 var author
 class TakeContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
 
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
 
     onRatingSet (newRating) {
         console.log("new rating for take " + this.props.take.id + ": " + newRating);
@@ -35,15 +48,47 @@ class TakeContainer extends Component {
         return (
 
             <div className="take">
-                <div>
-                    {this.findAuthor()}
-                    <strong>Take {this.props.count} by <font color="blue">{author}</font> on [date]</strong>
-                    <Star rating={this.props.take.rating} onChange={this.onRatingSet.bind(this)}/>
-                    <AudioComponent
-                        src={config.streamingUrl + this.props.take.location}
-                    />
-                    <Delete/>
-                </div>
+                {this.findAuthor()}
+                <strong>Take {this.props.count} by <font color="blue">{author}</font> on [date]</strong>
+
+                <Star rating={this.props.take.rating} onChange={this.onRatingSet.bind(this)}/>
+
+                <Grid columns={2} relaxed>
+                    <Grid.Column width={9}>
+
+                <AudioComponent
+                    src={config.streamingUrl + this.props.take.location}
+                />
+                <Delete/>
+                    </Grid.Column >
+
+                    <Divider vertical>Or</Divider>
+
+                    <Button onClick={(e) => this.handleClick(e)} content='Source Audio' icon='right arrow' labelPosition='right' />
+                    {this.state.isToggleOn ? '' :
+
+                        <Grid.Column width={5}>
+                        {/* the segment will be replaced with audio component?*/}
+                        <Segment basic>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
+                        </Segment>
+                    </Grid.Column>}
+
+                </Grid>
+
+
+                {/*<Card>*/}
+                    {/*{this.findAuthor()}*/}
+                    {/*<strong>Take {this.props.count} by <font color="blue">{author}</font> on [date]</strong>*/}
+                    {/*<Button onClick={(e) => this.handleClick(e)} content='Source Audio' icon='right arrow' labelPosition='right' />*/}
+                    {/*<Star rating={this.props.take.rating} onChange={this.onRatingSet.bind(this)}/>*/}
+                    {/*<AudioComponent*/}
+                        {/*src={config.streamingUrl + this.props.take.location}*/}
+                    {/*/>*/}
+                    {/*<Delete/>*/}
+                {/*</Card>*/}
+
+                {/*<Card fluid color='red' header='Option 1' >source audio</Card>*/}
             </div>
         );
     }
