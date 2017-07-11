777e8cf87c7de27edb72885b3e57cef484010835
nathanalbers <nathan.albers@covenant.edu>
2017-07-11 14:17:52 -0400

    Added sort function

--- src/js/pages/takes/ChapterContainer.js
+++ src/js/pages/takes/ChapterContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import ChunkList from "./ChunkList";
+import ChunkList from "./components/ChunkList";
 import LoadingDisplay from "js/components/LoadingDisplay.js";
 import axios from 'axios';
 import config from 'config/config';
@@ -8,6 +8,7 @@ import config from 'config/config';
 // this is the page for one chapter
 
 class ChapterContainer extends Component {
+
     constructor (props) {
         super(props);
         this.state = {loaded: false, retry: null, segments: [], mode: "", source: "", takeList: []};
@@ -36,119 +37,62 @@ class ChapterContainer extends Component {
 
     }
 
-    // Creates array containing one instance of each start verse
-    // needs to be rewritten
-    findChunks(placeHolderArr) {
+    findStartVerses(paramArr) { // creates array of each start verse
+        var returnArr = [];
+        for (let i = 0; i < paramArr.length; i++) {
+            returnArr[returnArr.length] = paramArr[i].take.startv
+        }
+        return (returnArr);
+    }
+
+    removeDuplicates(paramArr) { // removes duplicates from an array
+        var returnArr = [];
+        returnArr = paramArr.filter(function(item, pos) {
+            return paramArr.indexOf(item) === pos;
+        })
+
+        return (returnArr);
+    }
+
+    createArray(paramArr, segments) { // returns an array containing one array of takes for each segment
+
         var newArr = [];
-
-        for (let i = 0; i < placeHolderArr.length; i++) {
-
-            newArr[newArr.length] = placeHolderArr[i].startv
-        }
-
-        var uniqueArray = newArr.filter(function(item, pos) {
-            return newArr.indexOf(item) === pos;
-        })
-
-        return(
-            uniqueArray
-        );
-    }
-
-
-    // creates array containing each chunk and array of each take in that chunk
-    // needs to be rewritten
-    createChunkTakes(placeHolderArr) {
-        var uniqueArray = this.findChunks(placeHolderArr);
-        var finalArr = [];
-        for(let i = 0; i < uniqueArray.length; i++) {
-            var counter = 0;
-            var chunkArr = [];
-
-            for(let j = 0; j < placeHolderArr.length; j++) {
-                if (placeHolderArr[j].startv === uniqueArray[i]) {
-                    chunkArr[counter] = placeHolderArr[j];
-                    counter += 1;
-                }
-            }
-            finalArr[i] = chunkArr;
-        }
-
-        console.log('createChunkTakes() finished')
-        return (
-            finalArr
-        );
-    }
-
-
-
-    render () {
-        var segments = this.state.segments
-        {console.log('segments', segments)} // returns array of 23 objects
-
-        //var finalArr = this.createChunkTakes(segments)
-        //console.log('finalArr', finalArr)
-
-        // divide segments into array of arrays
-
-        var tempArr = [];
-        for (let i = 0; i < segments.length; i++) {
-            tempArr[tempArr.length] = segments[i].take.startv
-
-        }
-
-        console.log('tempArr', tempArr) // returns array of 23 start verses (includes duplicates)
-
-        //remove duplicates
-
-        tempArr = tempArr.filter(function(item, pos) {
-            return tempArr.indexOf(item) === pos;
-        })
-
-        console.log('tempArr2', tempArr) // returns array of 23 start verses (no duplicates)
-
-        // iterate through unique array (tempArr) and replace start verse with takes that include that start verse
-
-        for (let i = 0; i < tempArr.length; i++) {
-            var int = tempArr[i];
+        for (let i = 0; i < paramArr.length; i++) {
+            var int = paramArr[i];
             var placeHolderArr = [];
             for (let j = 0; j < segments.length; j++) {
                 if (int === segments[j].take.startv) {
                     placeHolderArr[placeHolderArr.length] = segments[j]
                 }
             }
-
-            tempArr[i] = placeHolderArr
+            newArr[i] = placeHolderArr
         }
+        return (newArr);
+    }
 
-        console.log('finalArr', tempArr) // returns array containing one array for ever start verse found (22)
+    sort(arr) { // simple sort function
+        return arr.sort(function(a, b) {
+            return a - b
+        })
+    }
+
+    render () {
+
+        var tempArr = this.findStartVerses(this.state.segments) // find start verses
+
+        tempArr = this.sort(tempArr) // sort by start verse
+        tempArr = this.removeDuplicates(tempArr) // remove duplicates
+        tempArr = this.createArray(tempArr, this.state.segments) // create array for ChunkList component
 
         return (
             <div>
                 <h1>Chapter {this.props.match.params.chid}</h1>
                 {tempArr.map(this.createChunkList)}
-
-                {/*
-                 {console.log('tempArr', tempArr[1])}
-                 <ChunkList
-                 segments={tempArr[1]}
-                 mode={'chunk'}
-                 number={5}
-                 />
-                 */}
             </div>
         );
     }
 
     createChunkList(arr) {
-        //console.log('New ChunkList started...')
-
-        /*
-         console.log(arr)
-         console.log('mode:', arr[0].take.mode)
-         console.log('num:', arr[0].take.startv)
-         */
-
 
         return(
             <div>
@@ -158,14 +102,11 @@ class ChapterContainer extends Component {
                     mode={arr[0].take.mode}
                     number={arr[0].take.startv}
                 />
-                {/*console.log('ChunkList finished')*/}
 
             </div>
-
         );
 
     }
-
 
 }
 
