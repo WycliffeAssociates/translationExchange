957ee243adc6b75938b55d8bb460d9bbfea87dc4
ericaz98 <ez8@rice.edu>
2017-07-12 17:00:26 -0400

    WIP

--- src/js/pages/projects/components/language_dropdown.js
+++ src/js/pages/projects/components/language_dropdown.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Dropdown } from 'semantic-ui-react'
 import axios from 'axios'
-import {bookOptions, languageOptions} from "./common";
 import config from 'config/config';
+
+
+
 
 class LanguageDropdown extends Component {
     /*
@@ -14,72 +16,102 @@ class LanguageDropdown extends Component {
         this.state = {
             loaded: false,
             error: "",
-            languages:[],
-        books:[],
-        version:[]};
+            languages: [],
+            books: [],
+            version: [],
+            projects: []
+        }
     }
 
     /*
      In componentDidMount, do the API request for the data and then use
      setState to put the data in state
      */
-    componentDidMount() {
-        this.setState({error: ""});
-
-        axios.post('http://172.19.145.91:8000/api/all_project/', {
-                params: {}
-            }
-        ).then(results => {
+        language() {
+            this.setState({error: ""});
+            axios.post('http://172.19.145.91:8000/api/all_project/', {
+                    params: {}
+                }
+            ).then(results => {
                 this.setState({
                     loaded: true,
                     projects: results.data
                 });
+            }).catch(exception => {
+                this.setState({error: exception});
+            });
+        }
+    book(){
+        this.setState({error: ""});
+        axios.post('http://172.19.145.91:8000/api/all_project/', {
+                params: {}
+            }
+        ).then(results => {
+            this.setState({
+                loaded: true,
+                projects: results.data
+            });
         }).catch(exception => {
             this.setState({error: exception});
         });
     }
 
+    version(){
+        this.setState({error: ""});
+        axios.post('http://172.19.145.91:8000/api/all_project/', {
+                params: {}
+            }
+        ).then(results => {
+            this.setState({
+                loaded: true,
+                projects: results.data
+            });
+        }).catch(exception => {
+            this.setState({error: exception});
+        });
 
-        //I would do a web request here...
-        //Just going to put fake data in state instead
-
-    render () {
-    if (this.state.projects) {
-
-        const listItems = this.state.projects.map((project) =>
-           <li> RIP</li>
-        );
-
-
-        // var languageOptions = this.state.projects.map(function (user) {
-        //     return {key: user.lang.name.toString, text: user.lang.name.toString, value: user.lang.name.toString}
-        // })
-        // }
-
-        console.log(listItems)
-}
-        return (
-            <div>
-                <Dropdown placeholder='Select Language'
-                          selection
-                          search
-                          options={languageOptions}
-                />
-                <Dropdown placeholder='Select Book'
-                          selection
-                          search
-                          options={bookOptions}
-                />
-                <Dropdown
-                    placeholder='Select Version'
-                    selection
-                    search
-                          options={languageOptions}
-                />
-            </div>
-        );
     }
 
+
+    render() {
+        if (this.state.projects) {
+
+            var languageOptions = this.state.projects.map(function (user) {
+                return {key: user.lang.name, text: user.lang.name, value: user.lang.name}
+            });
+
+            var bookOptions = this.state.projects.map(function (user) {
+                return {key: user.book[0].name, text: user.book[0].name, value: user.book[0].name}
+            });
+
+            var versionOptions = this.state.projects.map(function (user) {
+                return {key: user.version, text: user.version, value: user.version}
+            });
+
+
+            return (
+                <div>
+                    <Dropdown placeholder='Select Language'
+                              selection
+                              search
+                              options={languageOptions}
+                    />
+                    <Dropdown placeholder='Select Book'
+                              selection
+                              search
+                              options={bookOptions}
+                    />
+                    <Dropdown
+                        placeholder='Select Version'
+                        selection
+                        search
+                        options={versionOptions}
+                    />
+                </div>
+            );
+        }
+
+    }
 }
 
 export default LanguageDropdown;
