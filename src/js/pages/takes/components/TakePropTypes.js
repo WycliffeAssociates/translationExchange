88ac9edce4235baa1e0138d246b757ad9e7357bb
sheesania <sheesania@users.noreply.github.com>
2017-07-12 10:39:04 -0400

    splitting Take container + presentational components

--- src/js/pages/takes/components/TakePropTypes.js
+++ src/js/pages/takes/components/TakePropTypes.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 var TakePropTypes = PropTypes.shape(
     {
         id: PropTypes.number.isRequired,
-        audioSource: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
         author: PropTypes.string.isRequired,
         rating: PropTypes.number.isRequired,
         timestamp: PropTypes.string.isRequired,
