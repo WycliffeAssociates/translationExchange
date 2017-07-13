b74b179624d2d63d3d53ab19ce96b7a741e18590
sheesania <sheesania@users.noreply.github.com>
2017-07-13 10:08:34 -0400

    request projects listing from DB

--- src/js/pages/projects/ProjectsListContainer.js
+++ src/js/pages/projects/ProjectsListContainer.js
@@ -1,158 +1,62 @@
-/*
-    Here's an example container component that would handle requesting from the API,
-    parsing the data, and then passing it to a child presentation component.
-
-    All the components for the projects screens are grouped together in a "projects"
-    folder
-*/
-
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import QueryString from 'query-string';
 import ProjectsList from "./components/ProjectsList";
 import 'css/projects.css'
-import {Container, Header, Table} from "semantic-ui-react";
 import axios from 'axios';
+import config from 'config/config';
 import LanguageDropdown from "./components/language_dropdown";
 
 class ProjectsListContainer extends Component {
-    /*
-     In the constructor, set the state to being empty so the component
-     can render without errors before the API request finishes
-     */
+
     constructor(props) {
         super(props);
         this.state = {
-            languages: [],
-            books: [],
-            version: [],
-            projects: []
+            projects: [], //projects gotten from the database
+            currentProjectQuery: "" //what query was used to get those projects from the database
         };
-
-
-        /*
-         In componentDidMount, do the API request for the data and then use
-         setState to put the data in state
-         */
-
-
-        // componentDidMount() {
-        //
-        //     axios.post('http://172.19.145.91:8000/api/get_project/', {
-        //         params: {"language": "en-x-demo2", "book":"mark", "version":"reg"}
-        //
-        //     }).then(results => {
-        //         this.setState({
-        //             projects: results.data
-        //
-        //         })
-        //     });
-        //
-        // }
-
-
-        //         .then(results => {
-        //             console.log(results.data);
-        //             this.setState({
-        //                 projects: results.data
-        //             })
-        //         }
-        //
-        //         .catch(function (error) {
-        //             console.log(error);
-        //         });
-        // }
-        //
-        //     I would do a web request here...
-        //     Just going to put fake data in state instead
-
-        this.setState({
-            projects: [
-                {
-                    id: 16,
-                    book: "Matthew",
-                    language: "English",
-                    translationType: "Unlocked Literal Bible",
-                    percentFinished: 7,
-                    contributors: ["Alison ", "Erica"],
-                    dateModified: "20 June 2017 4:16 pm"
-                },
-                {
-                    id: 17,
-                    book: "Mark",
-                    language: "English",
-                    translationType: "Unlocked Dynamic Bible",
-                    percentFinished: 10,
-                    contributors: ["Alison"],
-                    dateModified: "20 March 2016 3:16 pm"
-                },
-                {
-                    id: 18,
-                    book: "Luke",
-                    language: "English",
-                    translationType: "Unlocked Dynamic Bible",
-                    percentFinished: 25,
-                    contributors: ["Juan"],
-                    dateModified: "3 June 2015 9:33 pm"
-                },
-                {
-                    id: 20,
-                    book: "Genesis",
-                    language: "Spanish",
-                    translationType: "Unlocked Literal Bible",
-                    percentFinished: 40,
-                    contributors: ["Michael"],
-                    dateModified: "20 June 2017 3:30 am"
-                },
-                {
-                    id: 18,
-                    book: "Exodus",
-                    language: "English",
-                    translationType: "Unlocked Literal Bible",
-                    percentFinished: 87,
-                    contributors: ["Dennis"],
-                    dateModified: "26 March 2017 12:04 pm"
-                },
-                {
-                    id: 18,
-                    book: "Leviticus",
-                    language: "Spanish",
-                    translationType: "Unlocked Literal Bible",
-                    percentFinished: 12,
-                    contributors: ["Nathan"],
-                    dateModified: "28 February  2017 8:35 am"
-                }
-
-            ]
-        });
     }
 
-    /*
-        In render, just render a child presentation component, passing it
-        the data as props
-     */
+     componentDidMount () {
+        //if query string isn't blank, then request projects here to initially populate projects list
+        if (this.props.location.search) {
+            this.requestProjects(this.props.location.search);
+        }
+     }
+
+     requestProjects (queryString) {
+        var query = QueryString.parse(queryString);
+        console.log(query);
+
+         axios.post(config.apiUrl + 'all_project/', query)
+         .then((results) => {
+             console.log(results);
+             this.setState({
+                 projects: results.data,
+                 currentProjectQuery: queryString
+             });
+         }).catch((exception) => {
+             console.log("ERROR");
+             console.dir(exception);
+         });
+     }
+
+    //if the project query string has changed, request projects
+    componentWillReceiveProps (nextProps) {
+        if (this.state.currentProjectQuery !== nextProps.location.search) {
+            this.requestProjects(nextProps.location.search);
+        }
+    }
+
+
     render () {
         return (
             <div>
-                    <LanguageDropdown/>
-
-                <Container fluid>
-                    <Header as='h1'>Available Projects</Header>
-
-                <Table selectable fixed>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Language</Table.HeaderCell>
-                            <Table.HeaderCell>Book</Table.HeaderCell>
-                            <Table.HeaderCell>Percent Complete</Table.HeaderCell>
-                            <Table.HeaderCell>More</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-
-
+                    <LanguageDropdown projects={this.state.projects}
+                                      queryString={this.state.currentProjectQuery}/>
                     <ProjectsList projects={this.state.projects}/>
-
-                </Table>
-
-                </Container>
+                <Link to={{ pathname: '/projects', search: "?language=en-x-demo2" }} >English</Link>
+                <Link to={{ pathname: '/projects', search: "?language=aad" }} >Amal</Link>
 
             </div>
         );
