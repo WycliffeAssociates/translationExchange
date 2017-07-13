deef8061e20b7bae620bc3f119a0e3176e75e450
ericaz98 <ez8@rice.edu>
2017-07-13 10:09:58 -0400

    Auto stash before rebase of "origin/filter-dropdowns"

--- src/js/pages/projects/components/language_dropdown.js
+++ src/js/pages/projects/components/language_dropdown.js
@@ -4,8 +4,6 @@ import axios from 'axios'
 import config from 'config/config';
 
 
-
-
 class LanguageDropdown extends Component {
     /*
      In the constructor, set the state to being empty so the component
@@ -19,15 +17,35 @@ class LanguageDropdown extends Component {
             languages: [],
             books: [],
             version: [],
-            projects: []
+            projects:[]
         }
-    }
 
-    /*
-     In componentDidMount, do the API request for the data and then use
-     setState to put the data in state
-     */
-        language() {
+
+        if (this.props.projects) {
+            console.log('askjdfklasj');
+            //put the languages from projects into state
+            this.setState({
+                loaded: true,
+                languages: this.props.projects.map(function (user) {
+                    return {key: user.lang.name, text: user.lang.name, value: user.lang.name} }),
+
+                books: this.props.projects.map(function (user) {
+                    return {key: user.book[0].name, text: user.book[0].name, value: user.book[0].name}  }),
+            });
+        }
+
+            //
+            //     books: this.props.projects.map(function (user) {
+            //     return {key: user.book[0].name, text: user.book[0].name, value: user.book[0].name}
+            // });
+
+            // this.setState({ books: this.props.projects.map(function (user) {
+            //     return {key: user.version, text: user.version, value: user.version}
+            // });
+
+
+        else {
+            // do a request and put into state then. this .setstate
             this.setState({error: ""});
             axios.post('http://172.19.145.91:8000/api/all_project/', {
                     params: {}
@@ -35,83 +53,78 @@ class LanguageDropdown extends Component {
             ).then(results => {
                 this.setState({
                     loaded: true,
-                    projects: results.data
+                    projects: results.data,
+
+                    languages: this.state.projects.map(function (user) {
+                        return {key: user.lang.name, text: user.lang.name, value: user.lang.name} }),
+
+                    books: this.state.projects.map(function (user) {
+                        return {key: user.book[0].name, text: user.book[0].name, value: user.book[0].name}  }),
                 });
             }).catch(exception => {
                 this.setState({error: exception});
             });
         }
-    book(){
-        this.setState({error: ""});
-        axios.post('http://172.19.145.91:8000/api/all_project/', {
-                params: {}
-            }
-        ).then(results => {
-            this.setState({
-                loaded: true,
-                projects: results.data
-            });
-        }).catch(exception => {
-            this.setState({error: exception});
-        });
     }
 
-    version(){
-        this.setState({error: ""});
-        axios.post('http://172.19.145.91:8000/api/all_project/', {
-                params: {}
-            }
-        ).then(results => {
-            this.setState({
-                loaded: true,
-                projects: results.data
-            });
-        }).catch(exception => {
-            this.setState({error: exception});
-        });
+    /*
+     In componentDidMount, do the API request for the data and then use
+     setState to put the data in state
+     */
+    // language(e) {
+    //     this.setState({error: ""});
+    //     axios.get('http://172.19.145.91:8000/api/languages/'
+    //     ).then(results => {
+    //         this.setState({
+    //             loaded: true,
+    //             languages: results.data,
+    //         });
+    //     }).catch(exception => {
+    //         this.setState({error: exception});
+    //     });
+    // }
+    //
+    // book(e) {
+    //     this.setState({error: ""});
+    //     axios.post('http://172.19.145.91:8000/api/books/'
+    //     ).then(results => {
+    //         this.setState({
+    //             loaded: true,
+    //             books: results.data
+    //         });
+    //     }).catch(exception => {
+    //         this.setState({error: exception});
+    //     });
+    // }
 
-    }
 
 
     render() {
-        if (this.state.projects) {
-
-            var languageOptions = this.state.projects.map(function (user) {
-                return {key: user.lang.name, text: user.lang.name, value: user.lang.name}
-            });
-
-            var bookOptions = this.state.projects.map(function (user) {
-                return {key: user.book[0].name, text: user.book[0].name, value: user.book[0].name}
-            });
-
-            var versionOptions = this.state.projects.map(function (user) {
-                return {key: user.version, text: user.version, value: user.version}
-            });
-
-
+            console.log(this.state.languages);
             return (
                 <div>
                     <Dropdown placeholder='Select Language'
                               selection
                               search
-                              options={languageOptions}
+                              options={this.state.languages}
+                              // onChange={this.language}
                     />
                     <Dropdown placeholder='Select Book'
                               selection
                               search
-                              options={bookOptions}
+                              options={this.state.books}
+                              //onChange={this.book}
                     />
-                    <Dropdown
-                        placeholder='Select Version'
-                        selection
-                        search
-                        options={versionOptions}
-                    />
+                    {/*<Dropdown*/}
+                        {/*placeholder='Select Version'*/}
+                        {/*selection*/}
+                        {/*search*/}
+                        {/*options={versionOptions}*/}
+                        {/*onChange={this.version}*/}
+                    {/*/>*/}
                 </div>
             );
         }
-
-    }
 }
 
 export default LanguageDropdown;
