14bf40207005595a94c8775f9ffc4d41969daa2f
Dennis Marchuk <dmarchuk@eagles.bridgewater.edu>
2017-07-17 16:39:00 -0400

    Started Delete Takes From Database
    Started Delete Takes From Database

--- src/js/pages/takes/components/Delete.js
+++ src/js/pages/takes/components/Delete.js
@@ -4,16 +4,23 @@
 import React from 'react';
 import {Button} from "semantic-ui-react";
 import { Icon } from 'semantic-ui-react'
+import axios from 'axios'
 
 
 class Delete extends React.Component{
     render(){
         return(
-            <Button icon color="red">
+            <Button icon color="red" onClick={DeleteElement}>
                 <Icon name='trash' size="large"/>
             </Button>
         )
     }
 }
+
+function DeleteElement(){
+    axios.delete()
+}
+
+
 
 export default Delete
