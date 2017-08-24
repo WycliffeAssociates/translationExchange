0aa7a7a1acb450b81c7ff3b6418fb48c7a96f0da
dilipMaharjan <dilipmaharjann@gmail.com>
2017-08-24 10:36:59 -0400

    implemented dnd

--- src/js/pages/takes/components/Take.js
+++ src/js/pages/takes/components/Take.js
@@ -1,213 +1,233 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import config from "config/config";
-import {Button, Grid, Segment, Card, Modal, Icon} from "semantic-ui-react";
-import TakeListenButton from './AddTake'
-import 'css/takes.css'
+import { Button, Grid, Segment, Card, Modal, Icon } from "semantic-ui-react";
+import TakeListenButton from "./AddTake";
+import "css/takes.css";
 import StitchTakesButton from "./StitchTakesButton";
 import TakeCommentsButton from "./comments/TakeCommentsButton";
 
-var listenCounter = 0
+var listenCounter = 0;
 
 class Take extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isToggleOn: true,
+			addButtonIcon: "plus",
+			showMarkers: false,
+			showMarkersColor: ""
+		};
+		// This binding is necessary to make `this` work in the callback
+		this.handleClick = this.handleClick.bind(this);
+		// this.showMarker = this.showMarker.bind(this);
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {isToggleOn: true, addButtonIcon: "plus", showMarkers: false, showMarkersColor: ""}
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
-        // this.showMarker = this.showMarker.bind(this);
-    }
+	handleClick() {
+		this.setState({ isToggleOn: !this.state.isToggleOn });
+	}
 
-    handleClick() {
-        this.setState({isToggleOn: !this.state.isToggleOn});
-    }
+	addToListen() {
+		this.props.addToListenList(this.props);
 
-    addToListen() {
-        this.props.addToListenList(this.props);
+		if (this.state.addButtonIcon !== "plus") {
+			this.setState({
+				addButtonIcon: "plus"
+			});
+		} else {
+			this.setState({
+				addButtonIcon: "minus"
+			});
+		}
+	}
 
-        if (this.state.addButtonIcon !== "plus") {
-            this.setState(
-                {
-                    addButtonIcon: "plus"
-                }
-            )
-        }
-        else {
-            this.setState(
-                {
-                    addButtonIcon: "minus"
-                }
-            )
-        }
-    }
+	moveLeft() {
+		if (this.props.take.is_publish) {
+			this.props.onMarkedForExportToggled();
+		} else if (this.props.take.rating > 1) {
+			this.props.onRatingSet(this.props.take.rating - 1);
+		}
+	}
 
-    moveLeft() {
-        if (this.props.take.is_publish) {
-            this.props.onMarkedForExportToggled();
-        } else if (this.props.take.rating > 1){
-            this.props.onRatingSet(this.props.take.rating - 1)
-        }
-    }
+	moveRight() {
+		if (this.props.take.rating >= 3) {
+			this.props.onMarkedForExportToggled();
+		} else if (this.props.take.rating < 1) {
+			this.props.onRatingSet(2);
+		} else {
+			this.props.onRatingSet(this.props.take.rating + 1);
+		}
+	}
 
-    moveRight() {
-        if (this.props.take.rating >= 3) {
-            this.props.onMarkedForExportToggled();
-        } else if (this.props.take.rating < 1) {
-            this.props.onRatingSet(2);
-        } else {
-            this.props.onRatingSet(this.props.take.rating + 1);
-        }
-    }
+	render() {
+		const markers = this.props.take.markers;
+		let showMarkers = this.state.showMarkers;
+		var file = [];
+		file[0] = {
+			src: config.streamingUrl + this.props.take.location
+		};
 
+		return (
+			<Segment>
+				<Grid textAlign="left">
+					<Grid.Row>
+						<Grid.Column verticalAlign="middle">
+							{this.props.take.rating > 1
+								? <Icon
+										className="hoverButton"
+										name="chevron left"
+										onClick={this.moveLeft.bind(this)}
+									/>
+								: ""}
+						</Grid.Column>
+						<Grid.Column width={12}>
+							<Grid.Row verticalAlign="top">
+								<Grid>
+									<Grid.Column width={11} floated="left">
+										<font size="3">
+											<strong>
+												Take {this.props.count} -{" "}
+											</strong>
+										</font>
+										<font size="2" color="grey">
+											{this.props.author
+												? this.props.author.name
+												: "Unknown Author"}
+										</font>
+									</Grid.Column>
+									<Grid.Column floated="right">
+										<StitchTakesButton
+											onClick={this.addToListen.bind(this)}
+											icon={this.state.addButtonIcon}
+										/>
+									</Grid.Column>
+								</Grid>
+							</Grid.Row>
 
-    render() {
-        const markers = this.props.take.markers;
-        let showMarkers = this.state.showMarkers;
-        var file = [];
-        file[0] = {
-            "src": config.streamingUrl + this.props.take.location
-        };
+							<Grid.Row>
+								{this.parseDate(this.props.take.date_modified)}
+							</Grid.Row>
+							<Grid.Row className="centerPlayButton">
+								<br />
+								<TakeListenButton
+									onClick={() =>
+										this.props.playTake(
+											this.props.take.location,
+											this.props.count,
+											this.props.chunkNumber,
+											this.props.author.name,
+											this.parseDate(this.props.take.date_modified),
+											this.props.take.markers
+										)}
+								/>
+							</Grid.Row>
+							<Grid.Row verticalAlign="bottom">
+								<br />
+								<TakeCommentsButton
+									take={this.props.take}
+									comments={this.props.comments}
+									onClickSave={this.props.onClickSave}
+									deleteComment={this.props.deleteComment}
+									loadingActive={this.props.active}
+									count={this.props.count}
+									chunkNumber={this.props.chunkNumber}
+								/>
+							</Grid.Row>
+						</Grid.Column>
 
-        return (
+						<Grid.Column verticalAlign="middle">
+							{this.props.take.is_publish
+								? ""
+								: <Icon
+										className="hoverButton"
+										name="chevron right"
+										onClick={this.moveRight.bind(this)}
+									/>}
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Segment>
+		);
+	}
 
+	parseDate(date) {
+		var noon = "am";
+		var dateArr = date.split("T");
+		var date = dateArr[0];
 
-            <Segment>
+		var time = dateArr[1].split(".");
+		time = time[0].split(":");
+		date = date.split("-");
+		switch (date[1]) {
+			case "01":
+				date[1] = "January";
+				break;
+			case "02":
+				date[1] = "February";
+				break;
+			case "03":
+				date[1] = "March";
+				break;
+			case "04":
+				date[1] = "April";
+				break;
+			case "05":
+				date[1] = "May";
+				break;
+			case "06":
+				date[1] = "June";
+				break;
+			case "07":
+				date[1] = "July";
+				break;
+			case "08":
+				date[1] = "August";
+				break;
+			case "09":
+				date[1] = "September";
+				break;
+			case "10":
+				date[1] = "October";
+				break;
+			case "11":
+				date[1] = "November";
+				break;
+			case "12":
+				date[1] = "December";
+				break;
+		}
 
-                <Grid textAlign="left">
-                    <Grid.Row>
-                        <Grid.Column verticalAlign="middle">
-                            {this.props.take.rating > 1
-                                ? <Icon className="hoverButton" name="chevron left" onClick={this.moveLeft.bind(this)}/>
-                                : ""
-                            }
-                        </Grid.Column>
+		var hour = parseInt(time[0]);
+		if (hour / 12 > -1) {
+			noon = "pm";
+		}
 
-                        <Grid.Column width={12}>
+		if (!(hour % 12 === 0)) {
+			hour %= 12;
+		}
 
-                            <Grid.Row verticalAlign="top">
-                                <Grid>
-                                    <Grid.Column width={11} floated="left">
-                                        <font size="3"><strong>Take {this.props.count} -  </strong></font>
-                                        <font size="2" color="grey">{this.props.author ? this.props.author.name : "Unknown Author" }</font>
-                                    </Grid.Column>
-                                    <Grid.Column floated="right">
-                                        <StitchTakesButton onClick={this.addToListen.bind(this)} icon={this.state.addButtonIcon}/>
-                                    </Grid.Column>
-                                </Grid>
-                            </Grid.Row>
-
-                            <Grid.Row>
-                                {this.parseDate(this.props.take.date_modified)}
-                            </Grid.Row>
-                            <Grid.Row className="centerPlayButton">
-                                <br />
-                                <TakeListenButton onClick={ () =>
-                                    this.props.playTake(this.props.take.location,
-                                        this.props.count,
-                                        this.props.chunkNumber,
-                                        this.props.author.name,
-                                        this.parseDate(this.props.take.date_modified),
-                                        this.props.take.markers)
-                                }
-                                />
-                            </Grid.Row>
-                            <Grid.Row verticalAlign="bottom">
-                                <br />
-                                <TakeCommentsButton  take={this.props.take}
-                                                     comments={this.props.comments}
-                                                     onClickSave={this.props.onClickSave}
-                                                     deleteComment={this.props.deleteComment}
-                                                     loadingActive={this.props.active}
-                                                     count={this.props.count}
-                                                     chunkNumber={this.props.chunkNumber}
-                                />
-                            </Grid.Row>
-                        </Grid.Column>
-
-                        <Grid.Column verticalAlign="middle">
-                            {this.props.take.is_publish
-                            ? ""
-                            : <Icon className="hoverButton" name="chevron right" onClick={this.moveRight.bind(this)}/>
-                        }</Grid.Column>
-
-                    </Grid.Row>
-                </Grid>
-
-            </Segment>
-
-        );
-    }
-
-    parseDate(date) {
-
-        var noon = 'am';
-        var dateArr = date.split('T');
-        var date = dateArr[0]
-
-        var time = dateArr[1].split('.')
-        time = time[0].split(':')
-        date = date.split('-')
-        switch (date[1]) {
-            case '01':
-                date[1] = 'January';
-                break;
-            case '02':
-                date[1] = 'February';
-                break;
-            case '03':
-                date[1] = 'March';
-                break;
-            case '04':
-                date[1] = 'April';
-                break;
-            case '05':
-                date[1] = 'May';
-                break;
-            case '06':
-                date[1] = 'June';
-                break;
-            case '07':
-                date[1] = 'July';
-                break;
-            case '08':
-                date[1] = 'August';
-                break;
-            case '09':
-                date[1] = 'September';
-                break;
-            case '10':
-                date[1] = 'October';
-                break;
-            case '11':
-                date[1] = 'November';
-                break;
-            case '12':
-                date[1] = 'December';
-                break;
-        }
-
-        var hour = parseInt(time[0])
-        if ((hour / 12) > -1) {
-            noon = 'pm'
-        }
-
-        if (!((hour % 12) === 0)) {
-            hour %= 12
-        }
-
-        return (date[1] + ' ' + date[2] + ', ' + date[0] + ' at ' + hour + ':' + time[1] + noon);
-    }
+		return (
+			date[1] +
+			" " +
+			date[2] +
+			", " +
+			date[0] +
+			" at " +
+			hour +
+			":" +
+			time[1] +
+			noon
+		);
+	}
 }
 
 Take.propTypes = {
-    count: PropTypes.number.isRequired,
-    take: PropTypes.object.isRequired,
-    author: PropTypes.string.isRequired,
-    onRatingSet: PropTypes.func.isRequired,
-    onMarkedForExportToggled: PropTypes.func.isRequired,
-    takeId: PropTypes.number.isRequired
+	count: PropTypes.number.isRequired,
+	take: PropTypes.object.isRequired,
+	author: PropTypes.string.isRequired,
+	onRatingSet: PropTypes.func.isRequired,
+	onMarkedForExportToggled: PropTypes.func.isRequired,
+	takeId: PropTypes.number.isRequired
 };
-
 
 export default Take;
--- src/js/pages/takes/components/TakeList.js
+++ src/js/pages/takes/components/TakeList.js
@@ -1,51 +1,107 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import TakeContainer from "../TakeContainer";
 import TakePropTypes from "./TakePropTypes";
-
+import update from "react/lib/update";
+import { DropTarget } from "react-dnd";
 var iterator;
 class TakeList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			takes: this.props.takes
+		};
+	}
+	pushTake(take) {
+		this.setState(
+			update(this.state, {
+				takes: {
+					$push: [take]
+				}
+			})
+		);
+	}
 
+	removeTake(index) {
+		this.setState(
+			update(this.state, {
+				takes: {
+					$splice: [[index, 1]]
+				}
+			})
+		);
+	}
 
-    render () {
+	moveTake(dragIndex, hoverIndex) {
+		const { takes } = this.state;
+		const dragTake = takes[dragIndex];
+		this.setState(
+			update(this.state, {
+				takes: {
+					$splice: [[dragIndex, 1], [hoverIndex, 0, dragTake]]
+				}
+			})
+		);
+	}
 
-        return (
-            <div>
-
-                {this.props.takes.map(this.createListItem.bind(this))}
-
-            </div>
-
-        );
-    }
-
-    createListItem (take) {
-
-        return (
-            <div>
-                <br />
-                <TakeContainer
-                    take={take} // one take
-                    count={take.order}
-                    source={take.source}
-                    chunkNumber={this.props.chunkNumber}
-                    addToListenList={this.props.addToListenList}
-                    patchTake={this.props.patchTake}
-                    deleteTake={this.props.deleteTake}
-                    updateChosenTakeForChunk={this.props.updateChosenTakeForChunk}
-                    onClickSave={this.props.onClickSave}
-                    deleteComment={this.props.deleteComment}
-                    playTake={this.props.playTake}
-                    active={this.props.active}
-                    mode={this.props.mode}
-                />
-            </div>
-        );
-    }
+	render() {
+		const style = {
+			width: "200px",
+			height: "700px",
+			border: "1px dashed gray"
+		};
+		const {takes}=this.state;
+		const { connectDropTarget } = this.props;
+		return connectDropTarget(
+			<div style={{ ...style }}>
+				{takes.map((take, i) => {
+					return (
+						<div>
+							<br />
+							<TakeContainer
+								take={take} // one take
+								count={take.order}
+								source={take.source}
+								chunkNumber={this.props.chunkNumber}
+								addToListenList={this.props.addToListenList}
+								patchTake={this.props.patchTake}
+								deleteTake={this.props.deleteTake}
+								updateChosenTakeForChunk={this.props.updateChosenTakeForChunk}
+								onClickSave={this.props.onClickSave}
+								deleteComment={this.props.deleteComment}
+								playTake={this.props.playTake}
+								active={this.props.active}
+								mode={this.props.mode}
+								key={i}
+								index={i}
+								listId={this.props.ratingToGet}
+								removeTake={this.removeTake.bind(this)}
+								moveTake={this.moveTake.bind(this)}
+							/>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 TakeList.propTypes = {
-    takes: PropTypes.arrayOf(TakePropTypes).isRequired
+	takes: PropTypes.arrayOf(TakePropTypes).isRequired
+};
+const takeTarget = {
+	drop(props, monitor, component) {
+		const { ratingToGet } = props;
+		const sourceObj = monitor.getItem();
+		if (ratingToGet!== sourceObj.listId) {
+			component.pushTake(sourceObj.take);
+		}
+		return { listId: ratingToGet };
+	}
 };
 
-export default TakeList;
+export default DropTarget("TakeContainer", takeTarget, (connect, monitor) => ({
+	connectDropTarget: connect.dropTarget(),
+	isOver: monitor.isOver(),
+	canDrop: monitor.canDrop()
+}))(TakeList);
