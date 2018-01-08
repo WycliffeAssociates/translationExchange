9f3e462b927718dd37f81d8d8f3fa6b79ceb55d9
dilipMaharjan <dilipmaharjann@gmail.com>
2018-01-08 11:13:58 -0500

    scroll view for takes

--- src/js/pages/takes/TakeList.js
+++ src/js/pages/takes/TakeList.js
@@ -80,10 +80,11 @@ class TakeList extends Component {
 
 	render() {
 		const style = {
-			minHeight: "231px"
+			maxHeight: "20em",
+			overflowY: "scroll"
 		};
 		const { takes } = this.props;
-	
+
 		const { connectDropTarget } = this.props;
 
 		return connectDropTarget(
