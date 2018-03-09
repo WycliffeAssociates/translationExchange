9467ab45ec5c0cb01416c4a5ab1eb929ebe8b5c9
Nathan Shanko <natibekele@gmail.com>
2018-03-09 12:05:54 -0500

    add onClick prop to buttons for functionality

--- src/js/pages/Login/components/RedoButton.js
+++ src/js/pages/Login/components/RedoButton.js
@@ -2,24 +2,26 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-export const RedoButton = () => {
-  return (
-    <Button> Redo <i className="fa fa-microphone" /> </Button>
-  );
+export default class RedoButton extends React.Component {
+  render() {
+    return (
+      <Button onClick={this.props.onClick}> Redo <i className="fa fa-microphone" /> </Button>
+    );
+  }
 
-};
+}
 
 const Button= styled.button`
   border-radius: 20px;
   color: #00C5FF;
   background: white;
-  padding: 0.75vw 4vw;
+  padding: 0.4vw 4vw;
   font-size: 1.5vw;
   font-weight: 100;
   border: solid;
   border-width: 2px;
   border-color: #00C5FF;
-  
+
   box-shadow: 3px 3px 3px rgba(0,0,0,0.5);
   cursor: pointer;
 `;
--- src/js/pages/Login/components/YesButton.js
+++ src/js/pages/Login/components/YesButton.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-export const YesButton = () => {
-  return (
-    <Button> Yes <i className="fa fa-check" /> </Button>
-  );
+export default class YesButton extends React.Component {
 
-};
+  render() {
+
+    return (
+      <Button onClick={this.props.onClick}> Yes <i className="fa fa-check" /> </Button>
+    );
+  }
+}
 
 const Button= styled.button`
   border-radius: 20px;
   color: white;
   background: linear-gradient(to bottom, #0076FF, #00C5FF);
-  padding: 0.75vw 4vw;
+  padding: 0.4vw 4vw;
   font-size: 1.5vw;
   font-weight: 100;
   border: none;
