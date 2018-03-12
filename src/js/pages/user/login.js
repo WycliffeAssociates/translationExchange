fc9e0a102818756b150a2d33c5e179b6812feec2
dilip <dilipmaharjann@gmail.com>
2018-03-12 08:57:51 -0400

    google issue

--- src/js/pages/user/login.js
+++ src/js/pages/user/login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
-import GitHubLogin from 'react-github-login';
+import GitHubLogin from '../../components/social-login/GitHubLogin';
+import GoogleLogin from '../../components/social-login/GoogleLogin';
+
 import axios from "axios";
 import config from "../../../config/config";
 
@@ -30,6 +32,16 @@ export default class Demo extends Component {
 
   onLoginSuccess (user) {
     return axios.post("https://localhost/api/login/social/token_user/github/",{clientId:'f5e981378e91c2067d41',redirectUri:'https://localhost/',code:user.code}).
+    then(response=>{
+      console.log(response);
+      localStorage.setItem('token',response.data.token);
+    }).catch(err=>{
+      console.log(err);
+    }
+    );
+  }
+  onGoogleLoginSuccess (user) {
+    return axios.post("https://localhost/api/login/social/token_user/google-oauth2/",{clientId:'654283088678-4m4eecv24f79fn9neid8v9846m0gdl49.apps.googleusercontent.com',redirectUri:'https://localhost/',code:user.code}).
     then(response=>{
       console.log(response);
       localStorage.setItem('token',response.data.token);
@@ -69,6 +81,10 @@ export default class Demo extends Component {
         <GitHubLogin clientId="f5e981378e91c2067d41"
           redirectUri="https://localhost" 
           onSuccess={this.onLoginSuccess}
+          onFailure={this.onLoginFailure}/>
+         <GoogleLogin clientId="654283088678-4m4eecv24f79fn9neid8v9846m0gdl49.apps.googleusercontent.com"
+          redirectUri="https://localhost" 
+          onSuccess={this.onGoogleLoginSuccess}
           onFailure={this.onLoginFailure}/>
       </div>
     )
