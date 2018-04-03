01c8326baa2d19a5901ac495de313c5ccd63b030
Pinto18 <pinto18.nd@gmail.com>
2018-04-03 16:29:16 -0400

    Changed import statement to configuration file in sourceAudioAction.test.js

--- src/__test__/integration_tests/actions/sourceAudioAction.test.js
+++ src/__test__/integration_tests/actions/sourceAudioAction.test.js
@@ -2,7 +2,8 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 //import { fetchAllSourceAudio } from '../../js/actions';
 import { fetchAllSourceAudio } from '../../../js/actions';
-import config from '../../config/config'
+//import config from '../../config/config'
+import config from '../../../config/config'
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
