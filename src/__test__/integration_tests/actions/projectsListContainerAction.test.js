3b365b5801999c24540916d1b4fd30e4326bceae
Pinto18 <pinto18.nd@gmail.com>
2018-04-12 14:15:21 -0400

    Commented tests that created false positives

--- src/__test__/integration_tests/actions/projectsListContainerAction.test.js
+++ src/__test__/integration_tests/actions/projectsListContainerAction.test.js
@@ -1,6 +1,5 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-//import { fetchAllProjects } from '../../js/actions';
 import { fetchAllProjects } from '../../../js/actions';
 
 const middlewares = [thunk];
--- src/__test__/integration_tests/actions/sourceAudioAction.test.js
+++ src/__test__/integration_tests/actions/sourceAudioAction.test.js
@@ -1,8 +1,6 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-//import { fetchAllSourceAudio } from '../../js/actions';
 import { fetchAllSourceAudio } from '../../../js/actions';
-//import config from '../../config/config'
 import config from '../../../config/config'
 
 const middlewares = [thunk]
