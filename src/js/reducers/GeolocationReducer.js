f05725e3d1ccb0d2fbf14c9846f72e2a1827a423
Juan Lopez <anton.lopez09@gmail.com>
2018-04-18 15:07:17 -0400

    db translation in process

--- src/js/reducers/GeolocationReducer.js
+++ src/js/reducers/GeolocationReducer.js
@@ -2,15 +2,15 @@ import { UPDATE_LANGUAGE } from '../actions/types';
 import language from '../../languages/textToDisplay.json'
 
 const INITIAL_STATE = { displayText: language.English
-                      };
+};
 
 export default (state = INITIAL_STATE, action) => {
 
-    switch (action.type){
+  switch (action.type){
 
-        case UPDATE_LANGUAGE:
-          return {
-            state,
+    case UPDATE_LANGUAGE:
+      return {
+        state,
             displayText: language[action.updatelanguage]
           };
 
