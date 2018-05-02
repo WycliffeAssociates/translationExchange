fe32f28cb606653a90e618d24c6bb4a36af92b1f
Pinto18 <pinto18.nd@gmail.com>
2018-05-02 16:12:41 -0400

    "Fixing directory problems in travis setup script"

--- set-up-travis.sh
+++ set-up-travis.sh
@@ -5,5 +5,5 @@ export TEBACKEND=$(pwd)/tE-backend
 export WORKSPACE=$TRAVIS_BUILD_DIR
 env | grep TEBACKEND
 env | grep WORKSPACE
-mkdir $TEBACKEND
-git pull https://github.com/WycliffeAssociates/tE-backend.git $TEBACKEND
+git pull https://github.com/WycliffeAssociates/tE-backend.git
+cd $WORKSPACE
