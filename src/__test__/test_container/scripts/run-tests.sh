ea6f7c062ee2cda071d78fae3a755b8c4b1a7bfc
Pinto18 <pinto18.nd@gmail.com>
2018-05-04 13:40:16 -0400

    Moved execution of tests to shell script for docker container's use

---
+++ src/__test__/test_container/scripts/run-tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+#This script is for running the tests for the environment defined in test-runner.yml
+#The environment variables used in this script are defined test-runner.env
+
+cd $PROJ_DIR
+npm install
+rm $AUDIO_LIB
+cp $AUDIO_SCRIPT $AUDIO_LIB
+npm run test:ci --silent
