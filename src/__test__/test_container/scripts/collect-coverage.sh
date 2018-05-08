6bbf4165059e58f21dce2d520f710ddfc6f3b3b4
Pinto18 <pinto18.nd@gmail.com>
2018-05-08 14:58:43 -0400

    Added running tests to travis

--- src/__test__/test_container/scripts/collect-coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#Script for collecting the coverage data from the tests being run
-#Environment variables are defined in test-runner.env
-#Please be sure to run run-tests.sh before using this script
-
-echo "********Collecting And Sending Coverage Information******"
-cat $COVERAGE_INFO | $COVERALLS 
