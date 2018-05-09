7410b10fd68caa32e8ab07dc16dcad998d86abfc
Nathan Shanko <natibekele@gmail.com>
2018-05-09 15:44:45 -0400

    remove console.log

--- src/js/store.js
+++ src/js/store.js
@@ -8,8 +8,6 @@ const middleware = composeWithDevTools(applyMiddleware(thunk, logger));
 
 const persistedState = loadState();
 const store = createStore(reducer, persistedState, middleware);
-console.log(store.getState().kanbanPage, 'THE STATE OF THE KANBAN PAGE');
-console.log(persistedState, 'THE STATE OF THE PERSISTED PAGE');
 store.subscribe(() => {
   saveState({kanbanPage: store.getState().kanbanPage});
 });
