b282484a5013f18ef9af349e0059bc034c4a3d56
Nathan Shanko <natibekele@gmail.com>
2018-05-09 14:38:59 -0400

    edit store to load localStorage

--- src/js/store.js
+++ src/js/store.js
@@ -3,5 +3,14 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import reducer from "./reducers/reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import {loadState, saveState} from './localState';
 const middleware = composeWithDevTools(applyMiddleware(thunk, logger));
-export default createStore(reducer, middleware);
+
+const persistedState = loadState();
+const store = createStore(reducer, persistedState, middleware);
+console.log(store.getState().kanbanPage, 'THE STATE OF THE KANBAN PAGE');
+console.log(persistedState, 'THE STATE OF THE PERSISTED PAGE');
+store.subscribe(() => {
+  saveState({kanbanPage: store.getState().kanbanPage});
+});
+export default store;
