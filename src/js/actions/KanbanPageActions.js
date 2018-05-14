e32a34fa5b3802cb901c9b8894ce6dcb34c3a0f1
Nathan Shanko <natibekele@gmail.com>
2018-05-14 14:50:58 -0400

    add comment

--- src/js/actions/KanbanPageActions.js
+++ src/js/actions/KanbanPageActions.js
@@ -141,7 +141,7 @@ export const patchTake = (
             chk.published_take = take;                // marked as published, update the published take inside the chunk obj
 
           }
-          else if (chk.id === chunkId && !patch.published
+          else if (chk.id === chunkId && !patch.published // unpublish take at chunk level
           && rating === 3 && isPublished ===true) {
             //
             chk.published_take = null;
