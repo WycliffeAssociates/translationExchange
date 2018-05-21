53b6ebe79e86887b8e2f040aa3abd1f2c869390c
Nathan Shanko <natibekele@gmail.com>
2018-05-21 11:34:15 -0400

    chages if/else to try catch block

--- src/js/getIllustrations.js
+++ src/js/getIllustrations.js
@@ -1,13 +1,13 @@
 export default function getIllustrations(bookSlug) {
 
-  if (bookSlug) {
+  try {
     return {
       picker: require(`../assets/Book_Illustrations/${bookSlug}_Picker.jpg`),
       sketch: require(`../assets/Book_Illustrations/${bookSlug}_Sketch_Medium.jpg`),
     };
   }
 
-  else {
+  catch (err) {
     return {
       picker: require('../assets/Book_Illustrations/default.jpg'),
       sketch: require('../assets/Book_Illustrations/default.jpg'),
