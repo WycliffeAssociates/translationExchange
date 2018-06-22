2f028c135b01925d1a5460732d0612f3f8738e11
Nathan Shanko <natibekele@gmail.com>
2018-06-22 11:11:45 -0400

    allow navigation in chapter review on card touch

--- src/js/pages/ChapterReview/components/ReviewColumn/SelectedTake.js
+++ src/js/pages/ChapterReview/components/ReviewColumn/SelectedTake.js
@@ -6,24 +6,58 @@ export default class SelectedTake extends React.Component {
 
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.isActive = this.isActive.bind(this);
   }
 
+  handleClick() {
+    const {updateActiveChunkIndex, activeChunkIndex,
+      index, resetTake, takesPlaying} = this.props;
+    updateActiveChunkIndex(activeChunkIndex, index, takesPlaying);
+    resetTake(true);
+  }
 
-  render() {
+  isActive() {
     const {take, updateActiveChunkIndex, resetPos,txt,
       activeChunkIndex, active, resetTake, selectedTakesLength} = this.props;
+    if (active) { //return the selected take without the onClick handler
+      return (
+        <Container>
+          <Take take={take} txt={txt}
+            updateActiveChunkIndex={updateActiveChunkIndex}
+            activeChunkIndex={activeChunkIndex}
+            active={active} selectedTakesLength={selectedTakesLength}
+            resetPos={resetPos}
+            resetTake={resetTake} />
+
+          <Comments active={active} txt={txt} comments= {take.publishedTake.comments} />
+
+        </Container>
+      );
+    }
+
+    else {
+      return (
+        <Container onClick={this.handleClick}>
+          <Take take={take} txt={txt}
+            updateActiveChunkIndex={updateActiveChunkIndex}
+            activeChunkIndex={activeChunkIndex}
+            active={active} selectedTakesLength={selectedTakesLength}
+            resetPos={resetPos}
+            resetTake={resetTake} />
+
+          <Comments active={active} txt={txt} comments= {take.publishedTake.comments} />
+
+        </Container>
+      );
+    }
+  }
+
+  render() {
+    let selectedTake = this.isActive();
+
     return (
-      <Container>
-        <Take take={take} txt={txt}
-          updateActiveChunkIndex={updateActiveChunkIndex}
-          activeChunkIndex={activeChunkIndex}
-          active={active} selectedTakesLength={selectedTakesLength}
-          resetPos={resetPos}
-          resetTake={resetTake} />
-
-        <Comments active={active} txt={txt} comments= {take.publishedTake.comments} />
-
-      </Container>
+      selectedTake
     );
   }
 
